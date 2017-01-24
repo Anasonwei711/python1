bab8b2fb85770b17663658bf10c356123a59cf14
Anasonwei711 <wangsiwei711@hotmail.com>
2017-01-24 22:34:30 +0800

    Add files via upload

---
+++ 从list删除元素.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-  
+L = ['Adam', 'Lisa', 'Bart', 'Paul']
+#直接执行L.pop()会直接返还被删除的最后一个元素
+L.pop()
+#list在pop()语句执行后删最后一个元素,因此必须填写pop(X)来删去确定位置上的数
---
+++ 数列输出不同方法.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#不同方式输出元组元素
+t = (0,1,2,3,4,5,6,7,8,9)
+print t
+#注意用tuple在括号前标注，range后最后一个数字比目标数字大1
+s = tuple(range(0,10))
+print s
